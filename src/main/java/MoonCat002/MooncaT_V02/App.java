2ea48ef6849e4f22d75ba3aea8469ebc5286ffa7
mhr <mehedihasanrony@Mehedis-MacBook-Air.local>
2024-08-26 16:57:24 +0600

    first commit

---
+++ src/main/java/MoonCat002/MooncaT_V02/App.java
@@ -0,0 +1,13 @@
+package MoonCat002.MooncaT_V02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
