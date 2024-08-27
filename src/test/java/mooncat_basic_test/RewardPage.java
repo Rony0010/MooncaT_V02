4eb31b16452a377900acc1f7e7cbb32fb74373bd
mhr <mehedihasanrony@Mehedis-MacBook-Air.local>
2024-08-27 17:16:17 +0600

    Reward Page checking code have been added

---
+++ src/test/java/mooncat_basic_test/RewardPage.java
@@ -0,0 +1,77 @@
+package mooncat_basic_test;
+
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class RewardPage {
+
+
+	public String baseURL = "https://www.mooncat.com/";
+	public WebDriver driver;
+	
+	@BeforeTest
+	public void Setup() {
+		
+		//Driver initialization
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        
+        //Navigate to the project website
+        driver.get(baseURL);  
+		
+	}
+	
+
+	public void Login() {
+		
+		driver.findElement(By.xpath("//*[@id=\"CustomerEmail\"]")).sendKeys("exdevqa@kineticbrands.com");
+        driver.findElement(By.xpath("//*[@id=\"CustomerPassword\"]")).sendKeys("12345678");
+   
+		//Click on the login button
+        driver.findElement(By.xpath("//*[@id=\"LoginButton\"]")).submit();
+          
+			
+		}
+	
+	
+	
+	@Test
+	public void Check_Reward_Functionality() {
+
+		//Click on the Reward Link
+		driver.findElement(By.xpath("//*[@id=\"shopify-section-footer\"]/footer/div/div[2]/div[1]/div/div[1]/ul/li[2]/a")).click();
+		
+		
+		//Click on the Signin Button on Reward page
+		driver.findElement(By.xpath("//*[@id=\"shopify-section-template--16005566464160__rewards_page_header\"]/div/div[2]/div/a")).click();
+		Login();
+		
+		
+		//Click on Shop All Link
+		driver.findElement(By.xpath("//*[@id=\"siteNav\"]/li[1]/a")).click();
+		
+				
+	}
+
+	
+	
+	@AfterTest
+	public void TearDown() throws InterruptedException {
+		
+		Thread.sleep(3000); //wait for 3 seconds before quit
+		
+		driver.close();
+		driver.quit();
+	}
+	
+
+}
