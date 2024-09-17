fad76e30f4617b34ea47c04c27b6e82d73067366
mhr <mehedihasanrony@Mehedis-MacBook-Air.local>
2024-09-17 21:22:46 +0600

    Update Currency Converter Code

--- src/test/java/mooncat_basic_test/Currency_Converter_test.java
+++ src/test/java/mooncat_basic_test/Currency_Converter_test.java
@@ -26,25 +26,70 @@ public class Currency_Converter_test {
 		
 	}
 	
-	@Test
-	public void Check_Currency_Converter() {
+	/*public void clickOnCurrencyMenu() {
 		
-		driver.findElement(By.xpath("//*[@id=\"Slide-template--15967084642464__featured_collection-1\"]/div/div/div[3]/div[1]/h3")).click();
 		driver.findElement(By.xpath("//*[@id=\"site-header\"]/ul[2]/li[3]")).click();
-		WebElement changeTextElement = driver.findElement(By.xpath("//*[@id=\"ProductInfo-template--15967085297824__main\"]/div[1]/div[2]/div/div[1]/span[2]/span"));
+		
+	}*/
+	
+	public void changeToUSD() {
+	
+		driver.findElement(By.xpath("//*[@id=\"site-header\"]/ul[2]/li[3]")).click();
+		driver.findElement(By.xpath("//*[@id=\"site-header\"]/ul[2]/li[3]/div/ul/li[1]")).click();
+		
+		System.out.println("changeToUSD");
+	
+	}
+	
+	public void verifyCurrencyConverter(String testFromCurrencyName, String testToCurrencyName, String changeCurrencyPath, String displayPricePath) {
+
+		//click on Currency Menu
+		driver.findElement(By.xpath("//*[@id=\"site-header\"]/ul[2]/li[3]")).click();
+
+		//change the currency
+		String changeCurrency = changeCurrencyPath; 
+		driver.findElement(By.xpath(changeCurrencyPath)).click();
+		
+		//get the full price with currency notation
+		WebElement displayPriceWithSymbool = driver.findElement(By.xpath(displayPricePath));
+
 		
 		//get the text of the element
-		String fullCadPrice = changeTextElement.getText();
+		String PriceWithSymbool = displayPriceWithSymbool.getText();
 		
 		//the word to search for
-		String expectedText = "CAD";
+		String expectedText = testToCurrencyName;
 		
 		// Check if the currency converter working for USD to CAD
-        if (fullCadPrice.contains(expectedText)) {
-            System.out.println("The Currency Covert for CAD working properly !!");
+        if (PriceWithSymbool.contains(expectedText)) {
+            System.out.println(testFromCurrencyName + " to " + testToCurrencyName +  " working properly !! \n");
         } else {
-            System.out.println("The Currency Converter is not working perfectly !!");
+            System.out.println(testFromCurrencyName + " to " + testToCurrencyName +  " not working properly !! \n");
         }
+		
+	}
+	
+	@Test
+	public void Check_Currency_Converter() throws InterruptedException {
+		
+		
+		//click on the Product
+		driver.findElement(By.xpath("//*[@id=\"Slide-template--15967084642464__featured_collection-1\"]/div/div/div[3]/div[1]/h3/a")).click();
+		
+		//clickOnCurrencyMenu();
+		
+		verifyCurrencyConverter("$USD", "$CAD", "//*[@id=\"site-header\"]/ul[2]/li[3]/div/ul/li[2]", "//*[@id=\"ProductInfo-template--15967085297824__main\"]/div[1]/div[2]/div/div[1]/span[2]/span");
+		
+		//Thread.sleep(3000);
+		
+		changeToUSD();
+		verifyCurrencyConverter("$USD", "£GBP", "//*[@id=\"site-header\"]/ul[2]/li[3]/div/ul/li[3]", "//*[@id=\"ProductInfo-template--15967085297824__main\"]/div[1]/div[2]/div/div[1]/span[2]/span");
+		
+		
+		changeToUSD();
+		verifyCurrencyConverter("$USD", "€EUR", "//*[@id=\"site-header\"]/ul[2]/li[3]/div/ul/li[4]", "//*[@id=\"ProductInfo-template--15967085297824__main\"]/div[1]/div[2]/div/div[1]/span[2]/span");
+		
+		
 		
 	}
 	
