e905b51310ebeafb269c551212a851f0c7285f8c
mhr <mehedihasanrony@Mehedis-MacBook-Air.local>
2024-09-18 12:17:19 +0600

    Update Currency Converter and added code for USD to EUR

--- src/test/java/mooncat_basic_test/Currency_Converter_test.java
+++ src/test/java/mooncat_basic_test/Currency_Converter_test.java
@@ -118,6 +118,58 @@ public class Currency_Converter_test {
 		
 	}
 	
+
+
+	
+	@Test(priority = 3)
+	public void USD_to_EUR_Converter() throws InterruptedException {
+		
+		//click on the mooncat logo
+		driver.findElement(By.xpath("//*[@id=\"site-header\"]/a")).click();
+		Thread.sleep(2000);
+		
+		//click on the Product
+		driver.findElement(By.xpath("//*[@id=\"Slide-template--15967084642464__featured_collection-1\"]/div/div/div[3]/div[1]/h3/a")).click();
+		//Thread.sleep(3000);
+		
+		
+		//click on currency menu
+		driver.findElement(By.xpath("//*[@id=\"site-header\"]/ul[2]/li[3]")).click();
+		Thread.sleep(5000);
+		
+
+		//change the currency to USD
+		driver.findElement(By.xpath("//*[@id=\"site-header\"]/ul[2]/li[3]/div/ul/li[1]")).click();
+		Thread.sleep(3000);
+		
+		//click on currency menu
+		driver.findElement(By.xpath("//*[@id=\"site-header\"]/ul[2]/li[3]")).click();
+		Thread.sleep(2000);
+				
+		
+		//change the currency to EUR
+		driver.findElement(By.xpath("//*[@id=\"site-header\"]/ul[2]/li[3]/div/ul/li[4]")).click();
+		Thread.sleep(2000);
+		
+		//get the price with currency notation
+		String gbpPrice =  driver.findElement(By.xpath("//*[@id=\"ProductInfo-template--15967085297824__main\"]/div[1]/div[2]/div/div[1]/span[2]")).getText();
+		System.out.println("\nProduct price in €EUR  : " +gbpPrice);
+		
+		//the word to search for
+		String expectedText = "EUR";
+		
+		// Check if the currency converter working
+        if (gbpPrice.contains(expectedText)) {
+            System.out.println("\n >> $USD to €EUR is working properly !! \n");
+        } else {
+            System.out.println("\n $USD to €EUR is not working properly !! \n");
+        }		
+		
+	}
+	
+		
+	
+	
 	
 	
 	@AfterTest
